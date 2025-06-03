f9fdf9e3a825cfc336c7d04dec500c7a48cf12d3
PedroHDantas <pedrohdantaspc@gmail.com>
2025-06-03 13:26:25 -0300

    feat (classe): adiciona classe Funcionario com suas propriedades

---
+++ Funcionario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modulo01EX02
+{
+    internal class Funcionario
+    {
+        public string Nome { get; set; }
+        public double SalarioBruto { get; set; }
+        public double  Imposto { get; set; }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,17 @@
-﻿namespace Modulo01EX02
+﻿using System.Runtime.ConstrainedExecution;
+
+namespace Modulo01EX02
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            //            Fazer um programa para ler os dados de um funcionário(nome,
+            // salário bruto e imposto). Em seguida, mostrar os dados do
+            //                funcionário(nome e salário líquido).Em seguida, aumentar o salário
+            //do funcionário com base em uma porcentagem dada(somente o
+            //salário bruto é afetado pela porcentagem) e mostrar novamente os
+            //dados do funcionário.Use a classe projetada abaixo.
         }
     }
 }
